a319df413a71a5e170223ba4f1c6e8fad9e2e34f
ibrahim BAVLI <31312276+ibavli@users.noreply.github.com>
2021-03-11 23:44:28 +0300

    Add project files.

---
+++ KaizenTechStudy/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KaizenTechStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
